352e1eb19dff4ece8c7557d78d5f964c69b6f616
EC2 Default User <ec2-user@ip-172-31-73-6.ec2.internal>
2022-10-04 21:16:18 +0000

    add docker stuff

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="benherman/bostonhousing"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy bostonhousing --image=$dockerpath:latest
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+podname=$(kubectl get pods | awk '{w=$1} END{print w}') 
+kubectl port-forward pod/$podname --address 0.0.0.0 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="benherman/bostonhousing"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag bostonhousing $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
